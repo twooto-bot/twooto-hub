c3b8f1ef5d25479217b78809b274aa45395775d8
twooto <66019453+twooto-bot@users.noreply.github.com>
2025-08-27 16:09:18 +0200

    Update twooto hub.lua

--- twooto hub.lua
+++ twooto hub.lua
@@ -1,12 +1,41 @@
 -- HUD variables --
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({
-    Name = "Twooto hub ❤️",
-    HidePremium = false,
-    SaveConfig = false,
-    ConfigFolder = "OrionLib",
-    IntroEnabled = true,
-    IntroText = "Welcome to twooto hub!",
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Twooto hub ❤️",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Welcome to twooto hub!",
+   LoadingSubtitle = "by twooto",
+   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "twooto hub"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/WYwXgHtdZW", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
 
 -- services --
@@ -27,7 +56,7 @@ if game.PlaceId == 2202352383 then -- spts classic
     -- Create a custom environment that includes your variables
     local env = getfenv()
     env.Window = Window
-    env.OrionLib = OrionLib
+    env.Rayfield = Rayfield
 
     -- Assign that environment to the chunk
     setfenv(chunk, env)
@@ -43,7 +72,7 @@ elseif game.PlaceId == 3823781113 then -- Saber Simulator
     -- Create a custom environment that includes your variables
     local env = getfenv()
     env.Window = Window
-    env.OrionLib = OrionLib
+    env.Rayfield = Rayfield
 
     -- Assign that environment to the chunk
     setfenv(chunk, env)
@@ -59,7 +88,7 @@ elseif game.PlaceId == 537413528 then
     -- Create a custom environment that includes your variables
     local env = getfenv()
     env.Window = Window
-    env.OrionLib = OrionLib
+    env.Rayfield = Rayfield
 
     -- Assign that environment to the chunk
     setfenv(chunk, env)
@@ -67,7 +96,7 @@ elseif game.PlaceId == 537413528 then
     -- Run the script
     chunk()
 else
-    OrionLib:MakeNotification({
+    Rayfield:MakeNotification({
         Name = "Error",
         Content = "This script is not supported in this game.",
         Image = "rbxassetid://4483345998",
