e2ed807ab71768b49bced2458b44839b0ec0aaf2
twooto <66019453+twooto-bot@users.noreply.github.com>
2025-08-30 13:33:22 +0200

    Update twooto hub.lua

--- twooto hub.lua
+++ twooto hub.lua
@@ -12,6 +12,23 @@ local Window = Rayfield:CreateWindow({
    DisableRayfieldPrompts = true,
    DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
+    ConfigurationSaving = {
+    Enabled = true,
+    FolderName = true, -- Create a custom folder for your hub/game
+    FileName = "Twooto Hub"
+    },
+
+    KeySystem = false, -- Set this to true to use our key system
+    KeySettings = {
+    Title = "twooto hub system",
+    Subtitle = "Key System",
+    Note = "compleet lootlabs to get key", -- Use this to tell the user how to get a key
+    FileName = "twooto_hubKey", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+    SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+    GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+    Key = {"twooto"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+
 })
 
 -- services --
@@ -131,3 +148,7 @@ plr.Idled:Connect(function()
     task.wait(1)
     VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
 end)
+
+
+-- load config --
+Rayfield:LoadConfiguration()
